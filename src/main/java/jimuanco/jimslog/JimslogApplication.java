bbbed921c1c73db32c53a52d42fa488b38e28ca8
Jimoo An <jmdh1004@gmail.com>
2023-11-30 20:05:17 +0900

    :tada: 프로젝트 시작

---
+++ src/main/java/jimuanco/jimslog/JimslogApplication.java
@@ -0,0 +1,13 @@
+package jimuanco.jimslog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JimslogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JimslogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/jimuanco/jimslog/JimslogApplicationTests.java
@@ -0,0 +1,13 @@
+package jimuanco.jimslog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JimslogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
