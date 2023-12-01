aeed86ac4c693b3abbf743e668fda0d9c9f099be
Jimoo An <jmdh1004@gmail.com>
2023-12-01 20:02:16 +0900

    :white_check_mark: 글 1개 조회 성공 Test 추가

--- src/test/java/jimuanco/jimslog/api/controller/post/PostControllerTest.java
+++ src/test/java/jimuanco/jimslog/api/controller/post/PostControllerTest.java
@@ -3,14 +3,20 @@ package jimuanco.jimslog.api.controller.post;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jimuanco.jimslog.api.controller.post.request.PostCreateRequest;
 import jimuanco.jimslog.api.service.post.PostService;
+import jimuanco.jimslog.api.service.post.response.PostResponse;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentMatchers;
+import org.mockito.BDDMockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.BDDMockito.*;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -84,5 +90,27 @@ class PostControllerTest {
                 .andExpect(jsonPath("$.code").value("400"))
                 .andExpect(jsonPath("$.message").value("잘못된 요청입니다."))
                 .andExpect(jsonPath("$.validation.content").value("내용을 입력해주세요."));
+    }
+    
+    @DisplayName("글을 1개 조회한다.")
+    @Test
+    void getPost() throws Exception {
+        // given
+        Long postId = 1L;
+
+        given(postService.getPost(anyLong()))
+                .willReturn(PostResponse.builder()
+                        .id(postId)
+                        .title("글제목 입니다.")
+                        .content("글내용 입니다.")
+                        .build()
+                );
+        // when // then
+        mockMvc.perform(get("/posts/{postId}", postId))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.data.id").value(1L))
+                .andExpect(jsonPath("$.data.title").value("글제목 입니다."))
+                .andExpect(jsonPath("$.data.content").value("글내용 입니다."));
     }
 }
