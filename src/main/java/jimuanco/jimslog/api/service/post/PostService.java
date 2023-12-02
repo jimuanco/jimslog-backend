daa4cb1aaefee20422899046d11706032e44e9aa
Jimoo An <jmdh1004@gmail.com>
2023-12-02 20:43:58 +0900

    :sparkles: 글 여러개 조회 API Controller 추가

--- src/main/java/jimuanco/jimslog/api/service/post/PostService.java
+++ src/main/java/jimuanco/jimslog/api/service/post/PostService.java
@@ -2,8 +2,11 @@ package jimuanco.jimslog.api.service.post;
 
 import jimuanco.jimslog.api.DataResponse;
 import jimuanco.jimslog.api.service.post.request.PostCreateServiceRequest;
+import jimuanco.jimslog.api.service.post.request.PostSearchServiceRequest;
 import jimuanco.jimslog.api.service.post.response.PostResponse;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class PostService {
@@ -12,6 +15,10 @@ public class PostService {
     }
 
     public PostResponse getPost(Long postId) {
+        return null;
+    }
+
+    public List<PostResponse> getPostList(PostSearchServiceRequest serviceRequest) {
         return null;
     }
 }
---
+++ src/main/java/jimuanco/jimslog/api/service/post/request/PostSearchServiceRequest.java
@@ -0,0 +1,23 @@
+package jimuanco.jimslog.api.service.post.request;
+
+import lombok.Builder;
+
+import static java.lang.Math.max;
+import static java.lang.Math.min;
+
+public class PostSearchServiceRequest {
+
+    private static final int MAX_SIZE = 2000;
+    private int page;
+    private int size;
+
+    @Builder
+    public PostSearchServiceRequest(int page, int size) {
+        this.page = page;
+        this.size = size;
+    }
+
+    public long getOffset() {
+        return (long) (max(1, page) - 1) * min(size, MAX_SIZE);
+    }
+}
