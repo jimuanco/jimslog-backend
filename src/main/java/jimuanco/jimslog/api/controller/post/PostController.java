e9a3283448f9956981835de5fa356b7207239121
Jimoo An <jmdh1004@gmail.com>
2023-12-14 20:59:35 +0900

    :lock: Post API 권한 설정

--- src/main/java/jimuanco/jimslog/api/controller/post/PostController.java
+++ src/main/java/jimuanco/jimslog/api/controller/post/PostController.java
@@ -4,10 +4,11 @@ import jakarta.validation.Valid;
 import jimuanco.jimslog.api.DataResponse;
 import jimuanco.jimslog.api.controller.post.request.PostCreateRequest;
 import jimuanco.jimslog.api.controller.post.request.PostEditRequest;
-import jimuanco.jimslog.api.controller.post.request.PostSearchRequest;
 import jimuanco.jimslog.api.service.post.PostService;
+import jimuanco.jimslog.api.service.post.request.PostSearchServiceRequest;
 import jimuanco.jimslog.api.service.post.response.PostResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -18,6 +19,7 @@ public class PostController {
 
     private final PostService postService;
 
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     @PostMapping("/posts")
     public void createPost(@Valid @RequestBody PostCreateRequest postCreateRequest) {
         postService.createPost(postCreateRequest.toServiceRequest());
@@ -29,16 +31,24 @@ public class PostController {
     }
 
     @GetMapping("/posts")
-    public DataResponse<List<PostResponse>> getPostList(@ModelAttribute PostSearchRequest postSearchRequest) {
-        return DataResponse.ok(postService.getPostList(postSearchRequest.toServiceRequest()));
+    public DataResponse<List<PostResponse>> getPostList( //todo 왜 ModelAttribute에 생성자 방식이 안먹히지?
+            @RequestParam(name = "page", required = false, defaultValue = "1") int page,
+            @RequestParam(name = "size", required = false, defaultValue = "10") int size) {
+        PostSearchServiceRequest serviceRequest = PostSearchServiceRequest.builder()
+                .page(page)
+                .size(size)
+                .build();
+        return DataResponse.ok(postService.getPostList(serviceRequest));
     }
 
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     @PatchMapping("/posts/{postId}")
     public void editPost(@PathVariable(name = "postId") Long postId,
                          @Valid @RequestBody PostEditRequest postEditRequest) {
         postService.editPost(postId, postEditRequest.toServiceRequest());
     }
 
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     @DeleteMapping("/posts/{postId}")
     public void deletePost(@PathVariable(name = "postId") Long postId) {
         postService.deletePost(postId);
