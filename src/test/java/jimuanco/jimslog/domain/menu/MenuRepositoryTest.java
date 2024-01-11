d2a00c76efe0b191b433dfdc128a79e4179f01c9
Jimoo An <jmdh1004@gmail.com>
2024-01-12 00:51:17 +0900

    :white_check_mark: Menu 조회 API MenuRepository Test 수정

--- src/test/java/jimuanco/jimslog/domain/menu/MenuRepositoryTest.java
+++ src/test/java/jimuanco/jimslog/domain/menu/MenuRepositoryTest.java
@@ -68,35 +68,43 @@ class MenuRepositoryTest {
     @Test
     void getMenusOrderedByListOrder() {
         // given
+        Menu mainMenu1 = Menu.builder()
+                .name("1. 메뉴")
+                .listOrder(1)
+                .children(new ArrayList<>())
+                .build();
+
         Menu subMenu1_1 = Menu.builder()
                 .name("1-1. 메뉴")
                 .listOrder(1)
                 .children(new ArrayList<>())
+                .parent(mainMenu1)
                 .build();
 
         Menu subMenu1_2 = Menu.builder()
                 .name("1-2. 메뉴")
                 .listOrder(2)
                 .children(new ArrayList<>())
+                .parent(mainMenu1)
                 .build();
 
-        Menu mainMenu1 = Menu.builder()
-                .name("1. 메뉴")
-                .listOrder(1)
-                .children(List.of(subMenu1_1, subMenu1_2))
+        mainMenu1.addChildren(subMenu1_1);
+        mainMenu1.addChildren(subMenu1_2);
+
+        Menu mainMenu2 = Menu.builder()
+                .name("2. 메뉴")
+                .listOrder(2)
+                .children(new ArrayList<>())
                 .build();
 
         Menu subMenu2_1 = Menu.builder()
                 .name("2-1. 메뉴")
                 .listOrder(1)
                 .children(new ArrayList<>())
+                .parent(mainMenu2)
                 .build();
 
-        Menu mainMenu2 = Menu.builder()
-                .name("2. 메뉴")
-                .listOrder(2)
-                .children(List.of(subMenu2_1))
-                .build();
+        mainMenu2.addChildren(subMenu2_1);
 
         Menu mainMenu3 = Menu.builder()
                 .name("3. 메뉴")
@@ -104,22 +112,46 @@ class MenuRepositoryTest {
                 .children(new ArrayList<>())
                 .build();
 
-        menuRepository.saveAll(List.of(mainMenu1, mainMenu2, mainMenu3));
+        menuRepository.saveAll(List.of(mainMenu2, mainMenu3, mainMenu1));
         em.clear();
 
         // when
         List<Menu> allMenus = menuRepository.findAllMenus();
+        Menu savedMainMenu1 = allMenus.stream()
+                .filter(menu -> menu.getName().equals("1. 메뉴"))
+                .findFirst()
+                .orElseThrow(() -> new RuntimeException("해당 메뉴가 존재하지 않습니다."));
+        Menu savedMainMenu2 = allMenus.stream()
+                .filter(menu -> menu.getName().equals("2. 메뉴"))
+                .findFirst()
+                .orElseThrow(() -> new RuntimeException("해당 메뉴가 존재하지 않습니다."));
+        Menu savedMainMenu3 = allMenus.stream()
+                .filter(menu -> menu.getName().equals("3. 메뉴"))
+                .findFirst()
+                .orElseThrow(() -> new RuntimeException("해당 메뉴가 존재하지 않습니다."));
 
         // then
-        assertThat(allMenus).hasSize(6)
+        assertThat(allMenus).hasSize(3)
                 .extracting("name", "listOrder")
                 .containsExactly(
                         tuple("1. 메뉴", 1),
-                        tuple("1-1. 메뉴", 1),
-                        tuple("2-1. 메뉴", 1),
-                        tuple("1-2. 메뉴", 2),
                         tuple("2. 메뉴", 2),
                         tuple("3. 메뉴", 3)
                 );
+
+        assertThat(savedMainMenu1.getChildren()).hasSize(2)
+                .extracting("name", "listOrder")
+                .containsExactly(
+                        tuple("1-1. 메뉴", 1),
+                        tuple("1-2. 메뉴", 2)
+                );
+
+        assertThat(savedMainMenu2.getChildren()).hasSize(1)
+                .extracting("name", "listOrder")
+                .contains(
+                        tuple("2-1. 메뉴", 1)
+                );
+
+        assertThat(savedMainMenu3.getChildren()).hasSize(0);
     }
 }
