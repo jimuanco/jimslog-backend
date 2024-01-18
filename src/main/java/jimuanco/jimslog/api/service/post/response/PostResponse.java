a86945a19ebcb2d74d4566ea4583e85b41046371
Jimoo An <jmdh1004@gmail.com>
2024-01-19 00:26:24 +0900

    :recycle: PostResponse 생성 메서드 createdDateTime 설정 추가

--- src/main/java/jimuanco/jimslog/api/service/post/response/PostResponse.java
+++ src/main/java/jimuanco/jimslog/api/service/post/response/PostResponse.java
@@ -4,18 +4,22 @@ import jimuanco.jimslog.domain.post.Post;
 import lombok.Builder;
 import lombok.Getter;
 
+import java.time.LocalDateTime;
+
 @Getter
 public class PostResponse {
 
     private Long id;
     private String title;
     private String content;
+    private LocalDateTime createdDateTime;
 
     @Builder
-    private PostResponse(Long id, String title, String content) {
+    private PostResponse(Long id, String title, String content, LocalDateTime createdDateTime) {
         this.id = id;
         this.title = title;
         this.content = content;
+        this.createdDateTime = createdDateTime;
     }
 
     public static PostResponse of(Post post) {
@@ -23,6 +27,7 @@ public class PostResponse {
                 .id(post.getId())
                 .title(post.getTitle())
                 .content(post.getContent())
+                .createdDateTime(post.getCreatedDateTime())
                 .build();
     }
 }
