877fa850b691732cf5ba2e6be5b29936acc31875
Jimoo An <jmdh1004@gmail.com>
2024-02-09 18:08:15 +0900

    :white_check_mark: Rest Docs Test 글 수정 API uploadImageUrls, deleteImageUrls 추가

--- src/test/java/jimuanco/jimslog/docs/post/PostControllerDocsTest.java
+++ src/test/java/jimuanco/jimslog/docs/post/PostControllerDocsTest.java
@@ -205,6 +205,8 @@ class PostControllerDocsTest extends RestDocsSupport {
                 .title("글제목을 수정했습니다.")
                 .content("글내용을 수정했습니다.")
                 .menuId(2)
+                .uploadImageUrls(List.of("URL1", "URL2"))
+                .deleteImageUrls(List.of("URL3", "URL4"))
                 .build();
         String json = objectMapper.writeValueAsString(request);
 
@@ -224,7 +226,11 @@ class PostControllerDocsTest extends RestDocsSupport {
                                 fieldWithPath("content").type(JsonFieldType.STRING)
                                         .description("글 내용"),
                                 fieldWithPath("menuId").type(JsonFieldType.NUMBER)
-                                        .description("수정할 메뉴 ID")
+                                        .description("수정할 메뉴 ID"),
+                                fieldWithPath("uploadImageUrls").type(JsonFieldType.ARRAY)
+                                        .description("최종적으로 등록할 Image URL"),
+                                fieldWithPath("deleteImageUrls").type(JsonFieldType.ARRAY)
+                                        .description("최종적으로 등록하지 않을 Image URL")
                         )
                 ));
     }
