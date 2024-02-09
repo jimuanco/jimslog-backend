58a2984b5dd86a31a179ba1b98787c38cfc8853e
Jimoo An <jmdh1004@gmail.com>
2024-02-09 18:01:41 +0900

    :sparkles: PostService 게시글 생성시 이미지 파일 S3,DB 처리 로직 추가

--- src/main/java/jimuanco/jimslog/api/service/post/PostService.java
+++ src/main/java/jimuanco/jimslog/api/service/post/PostService.java
@@ -1,5 +1,6 @@
 package jimuanco.jimslog.api.service.post;
 
+import com.amazonaws.services.s3.AmazonS3;
 import jimuanco.jimslog.api.service.post.request.PostCreateServiceRequest;
 import jimuanco.jimslog.api.service.post.request.PostEditServiceRequest;
 import jimuanco.jimslog.api.service.post.request.PostSearchServiceRequest;
@@ -7,13 +8,19 @@ import jimuanco.jimslog.api.service.post.response.PostResponse;
 import jimuanco.jimslog.domain.menu.Menu;
 import jimuanco.jimslog.domain.menu.MenuRepository;
 import jimuanco.jimslog.domain.post.Post;
+import jimuanco.jimslog.domain.post.PostImage;
+import jimuanco.jimslog.domain.post.PostImageRepository;
 import jimuanco.jimslog.domain.post.PostRepository;
 import jimuanco.jimslog.exception.MenuNotFound;
 import jimuanco.jimslog.exception.PostNotFound;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -23,15 +30,28 @@ import java.util.stream.Collectors;
 @Service
 public class PostService {
 
+    @Value("${cloud.aws.s3.bucket}")
+    private String bucket;
+
+    @Value("${jimslog.s3.url}")
+    private String s3Url;
+
     private final PostRepository postRepository;
     private final MenuRepository menuRepository;
+    private final PostImageRepository postImageRepository;
+    private final AmazonS3 amazonS3;
 
+    @Transactional
     public void createPost(PostCreateServiceRequest serviceRequest) {
         long menuId = serviceRequest.getMenuId();
         Menu menu = (menuId != 0) ? menuRepository.findById(menuId).orElseThrow(MenuNotFound::new) : null;
 
         Post post = serviceRequest.toEntity(menu);
         postRepository.save(post);
+
+        processImageUploadsForPost(serviceRequest.getUploadImageUrls(), post);
+        deleteS3Images(serviceRequest.getDeleteImageUrls());
+        deleteDbImages(serviceRequest.getDeleteImageUrls());
     }
 
     public PostResponse getPost(Long postId) {
@@ -74,5 +94,44 @@ public class PostService {
                 .orElseThrow(PostNotFound::new);
 
         postRepository.delete(post);
+    }
+
+    private void processImageUploadsForPost(List<String> uploadImageUrls, Post post) {
+        uploadImageUrls.stream()
+                .map(uploadImageUrl -> extractFileNameFromImageUrl(uploadImageUrl))
+                .forEach(fileName -> updatePostIdForPostImage(fileName, post.getId()));
+    }
+
+    private String extractFileNameFromImageUrl(String imageUrl) {
+        imageUrl.substring(s3Url.length() + 1);
+        String encodigString = imageUrl.substring(s3Url.length() + 1);
+        String decodingString;
+        try {
+            decodingString = URLDecoder.decode(encodigString, StandardCharsets.UTF_8.toString());
+        } catch (UnsupportedEncodingException e) {
+            throw new RuntimeException("Image URL Decoding 실패.");
+        }
+        return decodingString;
+    }
+
+    private void updatePostIdForPostImage(String fileName, Long postId) {
+        PostImage postImage = postImageRepository.findByFileName(fileName)
+                .orElseThrow(() -> new RuntimeException("이미지 파일이 존재하지 않습니다."));
+        postImage.updatePostId(postId);
+    }
+
+    private void deleteDbImages(List<String> deleteDbImageUrls) {
+        List<String> deleteFileNames = deleteDbImageUrls.stream()
+                .map(deleteImageUrl -> extractFileNameFromImageUrl(deleteImageUrl))
+                .collect(Collectors.toList());
+        postImageRepository.deleteAllByFileNameInQuery(deleteFileNames);
+    }
+
+    private void deleteS3Images(List<String> deleteImageUrls) {
+        deleteImageUrls
+                .forEach(deleteImageUrl -> {
+                    String FileName = extractFileNameFromImageUrl(deleteImageUrl);
+                    amazonS3.deleteObject(bucket, FileName);
+                });
     }
 }
