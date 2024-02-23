e06fd8e0636a752b73f38e35cfb532c42d1b8b96
Jimoo An <jmdh1004@gmail.com>
2024-02-24 01:22:12 +0900

    :wrench: cors 설정 삭제

--- src/main/java/jimuanco/jimslog/config/SecurityConfig.java
+++ src/main/java/jimuanco/jimslog/config/SecurityConfig.java
@@ -17,10 +17,6 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-
-import java.util.Collections;
 
 import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 
@@ -40,17 +36,6 @@ public class SecurityConfig {
                 .requestMatchers("/h2-console/**");
     }
 
-    CorsConfigurationSource corsConfigurationSource() {
-        return request -> {
-            CorsConfiguration config = new CorsConfiguration();
-            config.setAllowedHeaders(Collections.singletonList("*"));
-            config.setAllowedMethods(Collections.singletonList("*"));
-            config.setAllowedOriginPatterns(Collections.singletonList("https://jimslog-frontend.s3.ap-northeast-2.amazonaws.com/"));
-            config.setAllowCredentials(true);
-            return config;
-        };
-    }
-
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
@@ -64,8 +49,7 @@ public class SecurityConfig {
                 .exceptionHandling(e -> {
                     e.authenticationEntryPoint(new Http401Handler(objectMapper));
                     e.accessDeniedHandler(new Http403Handler(objectMapper));
-                })
-                .cors(corsConfigurer -> corsConfigurer.configurationSource(corsConfigurationSource()));
+                });
         return http.build();
     }
 
