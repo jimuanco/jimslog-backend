84cae281598a517807fc030feda7b527ee415a12
Jimoo An <jmdh1004@gmail.com>
2024-02-25 21:04:04 +0900

    :wrench: CORS Client Url 환경 변수로 처리

--- src/main/java/jimuanco/jimslog/config/WebMvcConfig.java
+++ src/main/java/jimuanco/jimslog/config/WebMvcConfig.java
@@ -1,5 +1,6 @@
 package jimuanco.jimslog.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -9,11 +10,14 @@ public class WebMvcConfig implements WebMvcConfigurer {
 
     private final long MAX_AGE_SECS = 3600;
 
+    @Value("${cors.client}")
+    private String corsClient;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         // 모든 경로에 대해
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins(corsClient)
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*") // 모든 헤더와
                 .allowCredentials(true) // 인증에 관한 정보도 허용
