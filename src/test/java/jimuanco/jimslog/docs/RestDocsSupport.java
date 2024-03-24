72153112a92ed20062e187ac22d1d77e7fd088d1
Jimoo An <jmdh1004@gmail.com>
2024-03-24 20:54:47 +0900

    :memo: AutoConfigureRestDocs uri 설정

--- src/test/java/jimuanco/jimslog/docs/RestDocsSupport.java
+++ src/test/java/jimuanco/jimslog/docs/RestDocsSupport.java
@@ -3,6 +3,7 @@ package jimuanco.jimslog.docs;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.restdocs.RestDocumentationContextProvider;
 import org.springframework.restdocs.RestDocumentationExtension;
 import org.springframework.test.web.servlet.MockMvc;
@@ -10,6 +11,7 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
 
+@AutoConfigureRestDocs(uriScheme = "https", uriHost = "api.jimuanco.com", uriPort = 443)
 @ExtendWith(RestDocumentationExtension.class)
 public abstract class RestDocsSupport {
 
